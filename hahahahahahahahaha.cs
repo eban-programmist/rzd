3de7eb4699a4d0eb8daed800f728e42ed53f5a8a
eban-programmist <pavelbuev98@yandex.ru>
2017-06-06 19:15:37 +0700

    LUL
    сматрите вот оно

---
+++ hahahahahahahahaha.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace rzd_eban_pogromist {
+    class Program {
+        static void Main(string[] args) {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int nomer_vagona = 0;
+            bool eban = true;
+            do {
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("    Вагон № {0:00}", nomer_vagona);
+                Console.Write("туалет свободен ●");
+                nomer_vagona++;
+                if (nomer_vagona == 31)
+                    nomer_vagona = 0;
+            }
+            while (eban);
+        }
+    }
+}
